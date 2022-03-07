4e88f0795c74ea1769afe4fc86cec05fc1d763e2
Lidiia-Afanasieva <cotaka@mail.ru>
2022-03-07 22:37:06 +0300

    непонятная функция reload_list

--- gamer.py
+++ gamer.py
@@ -6,8 +6,8 @@ import time
 class Gamer(object):
     cubes_value_list = [4, 6, 8, 10, 12, 20, 100]
 
-    def reload_list(self, dick, current_pool):
-        return [item for item in dick if item <= current_pool]
+    def reload_list(self, dick, pool_value_remainder):
+        return [item for item in dick if item <= pool_value_remainder]  # wtf its doing
 
     # @staticmethod
     # def heal_selection(self, coolman_pool_dict):
@@ -21,6 +21,7 @@ class Gamer(object):
 
     def __init__(self, pool_value):
         # self.coolman_pool_dict = coolm
+        self.dict_on_this_game_const = {}
         self.current_attack = 0
         self.current_block = 0
         self.personal_heal = 0
